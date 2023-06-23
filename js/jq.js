4b869638880d4afec74ef9c7f0089cd7321d922b
EkaterinaGomzyakova <kate.gm717@gmail.com>
2023-06-23 13:46:23 +0300

    upd

--- js/jq.js
+++ js/jq.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const html = $("html");
-  const canvas = $("#scroll_anim");
+  const canvas = $("#scroll_anim")[0];
   const context = canvas.getContext("2d");
 
   const currentFrame = index => (
@@ -40,5 +40,37 @@ $(document).ready(function() {
   };
 
   preloadImages();
-});
+
+
+	function toggleNav(){
+	  // if nav is open, close it
+	  if($("nav").is(":visible")){
+	    $("nav").fadeOut();
+	    $("button").removeClass("menu");
+	  }
+	  // if nav is closed, open it
+	  else{
+	    $("button").addClass("menu");
+	    $("nav").fadeIn().css('display', 'flex');
+	  }
+	}
+
+	// when clicking + button
+	$("button").click(function(){
+	  // when clicking ☰ button, open nav
+	  if($("header").hasClass("open")){
+	    toggleNav();
+	  }
+	  // when clicking + button, open header
+	  else{
+	    $("header").addClass("open");
+	  }
+	});
+	// close nav
+	$("#nav-close").click(function(){
+	  toggleNav();
+	});
+
+
+
 });
